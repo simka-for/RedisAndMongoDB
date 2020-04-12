94f6f86d568891aa830ef98e40d1a935868522b1
Simka-for <58855519+simka-for@users.noreply.github.com>
2020-04-12 20:18:54 +0300

    added connection to MongoDB

---
+++ src/main/java/MongoDB/MongoBook.java
@@ -0,0 +1,66 @@
+package MongoDB;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.BsonDocument;
+import org.bson.Document;
+
+import java.util.function.Consumer;
+
+public class MongoBook {
+
+    public static void main(String[] args) {
+
+        MongoClient mongoClient = new MongoClient( "127.0.0.1" , 27017 );
+        MongoDatabase database = mongoClient.getDatabase("local");
+
+        MongoCollection<Document> collection = database.getCollection("Books");
+        collection.drop();
+
+
+        Document firstBook = new Document()
+                .append("name", "Master and Margarita")
+                .append("author", "Bulgakov M.")
+                .append("date", 1966);
+
+        Document secondBook = new Document()
+                .append("name", "White guard")
+                .append("author", "Bulgakov M.")
+                .append("date", 1925);
+
+        Document thirdBook = new Document()
+                .append("name", "Crime and punishment")
+                .append("author", "Dostoevskiy F.")
+                .append("date", 1866);
+
+        Document fourthBook = new Document()
+                .append("name", "War and Piece")
+                .append("author", "Tolstoy L.")
+                .append("date", 1865);
+
+        Document fifthBook = new Document()
+                .append("name", "Evgeny Onegin")
+                .append("author", "Pushkin A.")
+                .append("date", 1832);
+
+        collection.insertOne(firstBook);
+        collection.insertOne(secondBook);
+        collection.insertOne(thirdBook);
+        collection.insertOne(fourthBook);
+        collection.insertOne(fifthBook);
+
+//        collection.find().forEach((Consumer<Document>) System.out::println);
+
+        BsonDocument firstQuery = BsonDocument.parse("{date: 1}");
+        BsonDocument secondQuery = BsonDocument.parse("{author: {$eq: \"Bulgakov M.\"}}");
+
+        collection.find().sort(firstQuery).limit(1).forEach((Consumer<Document>) document -> {
+            System.out.println("\nСамая старая книга:\n" + document);
+        });
+
+        System.out.println("Книги любимого писателя:");
+        collection.find(secondQuery).forEach((Consumer<Document>) System.out::println);
+    }
+
+}
